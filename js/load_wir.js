7d6b46e5351e4d0c9c971e1e1a892d7c27ea3e55
LuticaCANARD <presan100@gmail.com>
2022-03-27 14:57:05 +0900

    ;

--- js/load_wir.js
+++ js/load_wir.js
@@ -4,4 +4,5 @@ var big_post = {
     "title":document.getElementById("0post_title"),
     "text":document.getElementById("0post_text")};
 
-    big_post["pic"].src = "../posts/postDB/pics/vir.jpg"
+    big_post["pic"].src = "../posts/postDB/pics/vir.jpg";
+    
