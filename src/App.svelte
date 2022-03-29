a8907a7401e37d5b40d11a058dd87ba1e083a7e1
LuticaCANARD <presan100@gmail.com>
2022-03-29 20:29:07 +0900

    svl test

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 
 <script>
 	let name = 'world';
+	let image_test_src = "https://pbs.twimg.com/media/FOwLE96aQAAqBzU?format=jpg&name=large";
+	let image_test_src_2 = "https://pbs.twimg.com/media/FOwLEJbaAAEgzcG?format=jpg&name=large";
+	let testpic_list = [image_test_src,image_test_src_2]
+	let buttoncount = 0;
+	function change_pic(to_pic_src,id){
+		to_pic_src = id;
+	}
+	function button_fun(list_of_src,count){
+		if (count == list_of_src.lenght){
+			count = 0;
+		}else{
+			count++;
+		}
+	}
+
 </script>
 
-<h1>Hello {name}!</h1>
-<div>
-
-</div>
+<head>
+	
+</head>
+<body>
+	<img id="image_test" src={testpic_list[buttoncount]} alt="..." >
+	<button on:click={button_fun(testpic_list,buttoncount)}/>
+</body>
