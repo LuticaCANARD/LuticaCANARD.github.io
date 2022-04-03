6c202f04ffa8c882dcf4bf33deb7b608a48c56f3
LuticaCANARD <presan100@gmail.com>
2022-04-03 10:24:22 +0900

    depress

---
+++ src/post_list/tech.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let darkmode
+    export let fontmode
+</script> 
+<head>
+</head>
+    <body class={darkmode+fontmode}>
+    <hr>
+    <center>
+    <h3> 제목(기술) </h3>
+    </center>
+    <p1> 글 </p1>
+    <hr>
+</body>
