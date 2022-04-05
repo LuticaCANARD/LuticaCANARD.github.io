8c36d418141d9fe3befce21cfe9a605866cfd271
LuticaCANARD <presan100@gmail.com>
2022-04-06 06:42:46 +0900

    ?

--- src/post_list/nomallt.svelte
+++ src/post_list/nomallt.svelte
@@ -1,12 +1,12 @@
 <script>
     import postsk from "./postList.json"
-    import TEST_bed from "../src/TESTpost.svelte"
+    //import TEST_bed from "../src/TESTpost.svelte"
     export let darkmode
     export let fontmode
     export let code
     let input_text;
 
-    </script> 
+</script> 
     <head>
     </head>
     <body class={darkmode+fontmode}>
@@ -37,7 +37,7 @@
         <br>
         <h1> Write Post! </h1>
         <br>
-        <textarea cols = "80" row="33" bind:value = {input_text}></textarea>
+        <textarea cols = "55" row="55" bind:value = {input_text}></textarea>
         <br>
         <h3> return : </h3>
         <br>
