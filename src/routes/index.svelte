171c8c031171f08392a6dbab74d74c2e88c3ac38
LuticaCANARD <presan100@gmail.com>
2022-04-17 17:47:00 +0900

    ?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
