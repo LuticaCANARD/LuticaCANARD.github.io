209960fedbc7ff55f35a0bc29c0d15d1c0e1d71a
LuticaCANARD <presan100@gmail.com>
2022-04-17 15:33:45 +0900

    noje

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from "@sveltejs/adapter-static"; 
 // was "@sveltejs/adapter-auto"
 
-const dev = "production" === "development";
+//const dev = "production" === "development";
 
 /** @type {import(""@sveltejs/kit").Config} */
 const config = {
