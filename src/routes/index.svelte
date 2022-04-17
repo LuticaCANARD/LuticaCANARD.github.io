6f44524ebc63e87aaa87c16118446cb586af0c34
LuticaCANARD <presan100@gmail.com>
2022-04-17 14:50:45 +0900

    tet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,59 @@
-<svelte:head>
+<script context="module">
+	export const prerender = true;
+</script>
 
+<script>
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
 </svelte:head>
-<svelte:body>
-<p1> Lutica's blog is building now, sorry to this inconvenience</p1>
-</svelte:body>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
