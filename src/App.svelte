523682759306272b653136b2a026f2597de92943
LuticaCANARD <presan100@gmail.com>
2022-04-19 15:08:41 +0900

    up

--- src/App.svelte
+++ src/App.svelte
@@ -96,6 +96,6 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="../js/scripts.js"></script>
+        <script src="../public/js/scripts.js"></script>
     </body>
 </html>
