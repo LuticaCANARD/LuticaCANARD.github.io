501fe07fa0ed5e2d9b22e72cc6cffbeb64aa59f0
LuticaCANARD <presan100@gmail.com>
2023-03-06 19:08:26 +0900

    change profile

--- src/lib/profile.svelte
+++ src/lib/profile.svelte
@@ -1,16 +1,68 @@
 <script>
     import profilepic from '../assets/profile.png'
+    const history = [
+        {
+            name : "Lathion_project",
+            stacks : [
+                {
+                    name: "Python",
+                    imglink:"",
+                    desc: " SERVER "
+                },                
+                {
+                    name: "C# / Unity",
+                    imglink:"",
+                    desc: " Unity Client "
+                },
+                {
+                    name: "JS",
+                    imglink:"",
+                    desc: " Web Client "
+                },
+            ],
+            desc : "# projects to ..."
+        }
+    ]
+    import SvelteMarkdown from 'svelte-markdown'
+ 
 </script>
 <div id="profile">
     <center>
-        <h1>
-        Hi! , I'm Lutica!
-        </h1>
-        <h1>
-            "Here is under renovating now."
-        </h1>
-
-
+        <div>
+            <!-- 프로필 사진 -->
+            
+            <!-- 약력 -->
+            <div id="profile_history">
+                {#each history as story}
+                    <div class="history_element">
+                        <!--이름, 스택, 서술-->
+                        <div class="history_element_name">
+                            {story.name}
+                        </div>
+                        <div class="history_element_stack">
+                            {#each story.stacks as stack}
+                                <div class="stacks">
+                                    <div class="stacks_img">
+                                        <img src={stack.imglink}/>
+                                    </div>
+                                    <div class="stacks_name">
+                                        <h3>{stack.name}</h3>
+                                    </div>
+                                    <div class="stacks_desc">
+                                        <p1>{stack.desc}</p1>
+                                    </div>
+                                </div>
+                            {/each}
+                        </div>
+                        <div class="history_element_desc">
+                            <SvelteMarkdown source={story.desc}></SvelteMarkdown>
+                        </div>
+                    </div>
+                {/each}
+            </div>
+            <!-- 진행중인 프로젝트 -->
+            <!-- GITHUB Link 등 -->
+        </div>
     </center>
     <div style="text-align : right; ">
         <p1>
