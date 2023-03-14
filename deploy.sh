092d072904463b23ab942cb0fb3f8500885394e4
LuticaCANARD <presan100@gmail.com>
2023-03-14 14:44:11 +0900

    ed

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # https://<USERNAME>.github.io 에 배포
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:https://luticacanard.github.io.git main
 
 # https://<USERNAME>.github.io/<REPO> 에 배포
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+#git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
