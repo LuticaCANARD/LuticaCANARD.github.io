134c245d1b997d0c39ee22bc027dda75396a3ef2
LuticaCANARD <presan100@gmail.com>
2024-09-19 03:39:05 +0900

    dd

--- src/lib/routers/about/index.svelte
+++ src/lib/routers/about/index.svelte
@@ -4,13 +4,25 @@
   import { _  as i18n,isLoading } from 'svelte-i18n'
 </script>
 <style>
+.about-me{
 
+}
 </style>
 <div>
   {#if $isLoading}
     <Loading />
   {:else}
-    <h1>{$i18n('about-me.title')}</h1>
-    <p>{$i18n('about-me.p1')}</p>
+  <div>
+    <div class="about-me">
+      <div class="main-class">
+        <h1>{$i18n('about-me.title')}</h1>
+        <p>{$i18n('about-me.p1')}</p>
+        <p1>{$i18n('about-me.p2')}</p1>
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
   {/if}
 </div>
